526a7604f6407dec8ef6d83d91170c8ff999ea15
Mandy <34908590+picaq@users.noreply.github.com>
2020-12-27 22:38:35 -0800

    finish exercises with comments

--- prework/1_VariablesExercise.js
+++ prework/1_VariablesExercise.js
@@ -32,26 +32,30 @@
 // - a variable called num1 with a value greater than 1 and less than 10
 // - a variable called num2 with a value of greater than or equal to 1 and less
 // than or equal to 10
-
+    let num1 = 7;
+    let num2 = 1;
 
 
 // Exercise 6. Write a comment below explaining the difference between the rules
 // for num1 and num2 from Exercise 5. What numbers would be valid values for 
 // num2 but not num1?
-//
+    //  num1 strictly includes values 2-9 but excludes 1 and 10 whereas
+    //  num 2 includes values 1-10 and does not exlude the outer bounds because of the "or equal to" in the comparison
+
 
 
 // Exercise 7. Now we will try some addition.
 // a. Assign the the values of 4 and 6 to num1 and num2, respectively.
 // b. Make a new variable called 'theSum', and use 'num1' and
 // 'num2' to assign its value using the "+" operator.
-
-
+    let num1 = 4;
+    let num2 = 6;
+    let theSum = num1 + num2;
 
 // Exercise 8. Now we will try some multiplication.
 // Make a new variable called 'theProduct', multiply num1 and num2 and assign
 // the resulting value to 'theProduct'.
-
+    let theProduct = num1 * num2;
 
 
 // Exercise 9.
@@ -59,7 +63,7 @@
 // 'dreamDestination' and the concatenation method of your choice, make the
 // value of statement to be:
 // "Hi, my name is <your name>, and I can't wait to visit <your destination>!"
-
+    let statement = `Hi, my name is ${myName}, and I can't wait to visit ${dreamDestination}!`
 
 
 // *NOTE* For the following exercises, put your answers in the line below
@@ -71,17 +75,17 @@
 // Exercise 10. What is the data type of the value stored in 'dreamDestination',
 // place your answer in the quotes below:
 
-const myAnswerForExercise10 = "";
+const myAnswerForExercise10 = "string";
 
 // Exercise 11. What is the data type of the value stored in 'product',
 // place your answer in the quotes below:
 
-const myAnswerForExercise11 = "";
+const myAnswerForExercise11 = "number";
 
 // Exercise 12. What do you think the value of emptyVariable is?
 // Place your answer below:
-
-const myAnswerForExercise12 = ;
+    // I thought it would be null but I tested it out in the console and it is undefined
+const myAnswerForExercise12 = undefined;
 
 // Exercise 13. If
 const A = "R";
@@ -90,41 +94,43 @@ const C = 4;
 const D = "D";
 
 // What is the value of B + C
-const myAnswerForExercise13 = ;
+const myAnswerForExercise13 = 5;
 
 // Exercise 14 What is the value of
 // A + "n" + D
-const myAnswerForExercise14 = ;
+const myAnswerForExercise14 = "RnD";
 
 // Exercise 15 What is the value of
 // A + B * 2 + D + C / 2:
-const myAnswerForExercise15 = ;
+const myAnswerForExercise15 = "R2D2";
 
 // Exercise 16 What is the value of A * B
-const myAnswerForExercise16 = ;
+const myAnswerForExercise16 = NaN;
 
 // Exercise 17 What is the value of true || false
-const myAnswerForExercise17 = ;
+const myAnswerForExercise17 = true;
 
 // Exercise 18 What is the value of 1 === "1"
-const myAnswerForExercise18 = ;
+const myAnswerForExercise18 = false;
 
 // Exercise 19 What is the value of 1 == "1"
-const myAnswerForExercise19 = ;
+const myAnswerForExercise19 = true;
 
 // Exercise 20 What is the value of 1 < 1
-const myAnswerForExercise20 = ;
+const myAnswerForExercise20 = false;
 
 // Exercise 21 What is the value of 1 < 2 < 3
-const myAnswerForExercise21 = ;
+const myAnswerForExercise21 = true;
 
 // Exercise 22 What is the value of 2 < 1 < 3
-const myAnswerForExercise22 = ;
+const myAnswerForExercise22 = true;
 
 // Congrats, you made it to the end!
 // Did you find this easy or hard? If you used references, which ones helped you? 
 // Please answer in a comment below.
-//
+// This is easy. 
+// I used MDN to check if javascript separated numbers into integers and floats or if they are just number data types
+// I used the chrome console to check my answers and commented if my expected answer was different than the actual answer
 
 // Email your file to the course staff, 
 // or commit your file to GitHub and email us a link.
