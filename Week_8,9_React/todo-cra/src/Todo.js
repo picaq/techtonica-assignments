d00ce39fb6babcb754be9526ecd276f45ecdcb28
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-15 13:58:27 -0700

    Add checkboxes with labels in Todo

--- Week_8,9_React/todo-cra/src/Todo.js
+++ Week_8,9_React/todo-cra/src/Todo.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 class Todo extends React.Component {
-    render() {
-        const { todo } = this.props;
-        return <div>{todo.text}</div>;    
-    }
+  render() {
+    const { todo } = this.props;
+    const { text, isCompleted } = todo;
+    return (
+      <div>
+        <label><input type="checkbox" checked={isCompleted}></input>
+        {text}</label>
+      </div>
+    );
+  }
 }
 export default Todo;
