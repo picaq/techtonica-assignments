0b888633cf0d69daabebc6e4c5a8d95f143da1a2
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-15 11:54:33 -0700

    init Todo.js component with {todo.txt}

---
+++ Week_8,9_React/todo-cra/src/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Todo extends React.Component {
+    render() {
+        const { todo } = this.props;
+        return <div>{todo.text}</div>;    
+    }
+}
+export default Todo;
