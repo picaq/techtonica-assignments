8bb9a322705a20cae4cbfd36b5db92a00f8ad826
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-15 12:02:03 -0700

    replace App.js with Todo content

--- Week_8,9_React/todo-cra/src/App.js
+++ Week_8,9_React/todo-cra/src/App.js
@@ -1,26 +1,60 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         <p>Hello Techtonica!</p>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import Todo from './Todo';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hello Techtonica!</p>
-      </header>
-    </div>
-  );
+// class Todo extends React.Component {
+//   render() {
+//     const { todo } = this.props;
+//     return <div>{todo.text}</div>;
+//   }
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [{ text: 'Walk dog' }, { text: 'Feed cat' }]
+    };
+  }
+  render() {
+    const { todos } = this.state;
+    return (
+      <div className="App">
+        <h1>Todos</h1>
+        <div>
+          {todos.length &&
+            todos.map((todo, idx) => <Todo key={idx} todo={todo} />)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
